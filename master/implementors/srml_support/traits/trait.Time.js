5c13b23c32eca352bb93ef5ce7b290693c1173ae
devops-parity <devops-team@parity.io>
2019-07-24 17:55:36 +0000

    update rustdoc master

---
+++ master/implementors/srml_support/traits/trait.Time.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["srml_timestamp"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"srml_timestamp/trait.Trait.html\" title=\"trait srml_timestamp::Trait\">Trait</a>&gt; <a class=\"trait\" href=\"srml_support/traits/trait.Time.html\" title=\"trait srml_support::traits::Time\">Time</a> for <a class=\"struct\" href=\"srml_timestamp/struct.Module.html\" title=\"struct srml_timestamp::Module\">Module</a>&lt;T&gt;",synthetic:false,types:["srml_timestamp::Module"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
