0c17d829e5dd9dbf257645044b218c3fa231adc8
devops-parity <devops-team@parity.io>
2019-08-08 13:54:15 +0000

    update rustdoc master

--- master/substrate_test_runtime/genesismap/sidebar-items.js
+++ master/substrate_test_runtime/genesismap/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["additional_storage_with_genesis",""]],"struct":[["GenesisConfig","Configuration of a general Substrate test genesis block."]]});
+initSidebarItems({"fn":[["additional_storage_with_genesis",""],["insert_genesis_block",""]],"struct":[["GenesisConfig","Configuration of a general Substrate test genesis block."]]});
