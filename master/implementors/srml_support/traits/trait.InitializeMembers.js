9e6839f2a38c8950742a2e700b2a552a78d43aef
devops-parity <devops-team@parity.io>
2019-08-13 19:11:12 +0000

    update rustdoc master

---
+++ master/implementors/srml_support/traits/trait.InitializeMembers.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["srml_collective"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"srml_collective/trait.Trait.html\" title=\"trait srml_collective::Trait\">Trait</a>&lt;I&gt;, I:&nbsp;<a class=\"trait\" href=\"srml_collective/trait.Instance.html\" title=\"trait srml_collective::Instance\">Instance</a>&gt; <a class=\"trait\" href=\"srml_support/traits/trait.InitializeMembers.html\" title=\"trait srml_support::traits::InitializeMembers\">InitializeMembers</a>&lt;&lt;T as <a class=\"trait\" href=\"srml_system/trait.Trait.html\" title=\"trait srml_system::Trait\">Trait</a>&gt;::<a class=\"type\" href=\"srml_system/trait.Trait.html#associatedtype.AccountId\" title=\"type srml_system::Trait::AccountId\">AccountId</a>&gt; for <a class=\"struct\" href=\"srml_collective/struct.Module.html\" title=\"struct srml_collective::Module\">Module</a>&lt;T, I&gt;",synthetic:false,types:["srml_collective::Module"]},];
+implementors["srml_support"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
