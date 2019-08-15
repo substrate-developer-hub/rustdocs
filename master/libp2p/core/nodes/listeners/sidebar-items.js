24b43dd020e7aa07508300500b3dd0fbdd5f97e2
devops-parity <devops-team@parity.io>
2019-08-15 15:26:45 +0000

    update rustdoc master

--- master/libp2p/core/nodes/listeners/sidebar-items.js
+++ master/libp2p/core/nodes/listeners/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["ListenersEvent","Event that can happen on the `ListenersStream`."]],"struct":[["ListenerId","The ID of a single listener."],["ListenersStream","Implementation of `futures::Stream` that allows listening on multiaddresses."]]});
+initSidebarItems({"enum":[["ListenersEvent","Event that can happen on the `ListenersStream`."]],"struct":[["ListenersStream","Implementation of `futures::Stream` that allows listening on multiaddresses."]]});
