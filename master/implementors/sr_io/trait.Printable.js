6221f7d3e21bb1c018490624892a2d03192216a3
devops-parity <devops-team@parity.io>
2019-09-04 14:55:51 +0000

    update rustdoc master

--- master/implementors/sr_io/trait.Printable.js
+++ master/implementors/sr_io/trait.Printable.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["sr_io"] = [];
+implementors["sr_primitives"] = [{text:"impl <a class=\"trait\" href=\"sr_io/trait.Printable.html\" title=\"trait sr_io::Printable\">Printable</a> for <a class=\"struct\" href=\"sr_primitives/struct.DispatchError.html\" title=\"struct sr_primitives::DispatchError\">DispatchError</a>",synthetic:false,types:["sr_primitives::DispatchError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ master/implementors/sr_primitives/traits/trait.ModuleDispatchError.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["sr_primitives"] = [];
+implementors["srml_system"] = [{text:"impl <a class=\"trait\" href=\"sr_primitives/traits/trait.ModuleDispatchError.html\" title=\"trait sr_primitives::traits::ModuleDispatchError\">ModuleDispatchError</a> for <a class=\"enum\" href=\"srml_system/enum.Error.html\" title=\"enum srml_system::Error\">Error</a>",synthetic:false,types:["srml_system::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
