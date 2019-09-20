4ae679b67cd6af6382590ad6b8a1d44a0b73a5ea
devops-parity <devops-team@parity.io>
2019-09-20 14:00:51 +0000

    update rustdoc master

--- master/implementors/futures_core/stream/trait.TryStream.js
+++ master/implementors/futures_core/stream/trait.TryStream.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
 implementors["futures_core"] = [];
-implementors["futures_timer"] = [{text:"impl&lt;S&gt; <a class=\"trait\" href=\"futures_core/stream/trait.TryStream.html\" title=\"trait futures_core::stream::TryStream\">TryStream</a> for <a class=\"struct\" href=\"futures_timer/ext/struct.TimeoutStream.html\" title=\"struct futures_timer::ext::TimeoutStream\">TimeoutStream</a>&lt;S&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;S: <a class=\"trait\" href=\"futures_core/stream/trait.TryStream.html\" title=\"trait futures_core::stream::TryStream\">TryStream</a>,<br>&nbsp;&nbsp;&nbsp;&nbsp;S::<a class=\"type\" href=\"futures_core/stream/trait.TryStream.html#associatedtype.Error\" title=\"type futures_core::stream::TryStream::Error\">Error</a>: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/std/io/error/struct.Error.html\" title=\"struct std::io::error::Error\">Error</a>&gt;,&nbsp;</span>",synthetic:false,types:["futures_timer::ext::TimeoutStream"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
