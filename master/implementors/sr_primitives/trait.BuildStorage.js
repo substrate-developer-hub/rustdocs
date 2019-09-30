d7404bdcb467d5c76a1d4fe040d4e8a4d5537167
devops-parity <devops-team@parity.io>
2019-09-30 16:41:09 +0000

    update rustdoc master

--- master/implementors/sr_primitives/trait.BuildStorage.js
+++ master/implementors/sr_primitives/trait.BuildStorage.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["sr_primitives"] = [];
-implementors["substrate_service"] = [{text:"impl&lt;'a, G:&nbsp;<a class=\"trait\" href=\"substrate_service/trait.RuntimeGenesis.html\" title=\"trait substrate_service::RuntimeGenesis\">RuntimeGenesis</a>&gt; <a class=\"trait\" href=\"sr_primitives/trait.BuildStorage.html\" title=\"trait sr_primitives::BuildStorage\">BuildStorage</a> for &amp;'a <a class=\"struct\" href=\"substrate_service/struct.ChainSpec.html\" title=\"struct substrate_service::ChainSpec\">ChainSpec</a>&lt;G&gt;",synthetic:false,types:["substrate_service::chain_spec::ChainSpec"]},];
+implementors["substrate_chain_spec"] = [{text:"impl&lt;'a, G:&nbsp;<a class=\"trait\" href=\"substrate_chain_spec/trait.RuntimeGenesis.html\" title=\"trait substrate_chain_spec::RuntimeGenesis\">RuntimeGenesis</a>, E&gt; <a class=\"trait\" href=\"sr_primitives/trait.BuildStorage.html\" title=\"trait sr_primitives::BuildStorage\">BuildStorage</a> for &amp;'a <a class=\"struct\" href=\"substrate_chain_spec/struct.ChainSpec.html\" title=\"struct substrate_chain_spec::ChainSpec\">ChainSpec</a>&lt;G, E&gt;",synthetic:false,types:["substrate_chain_spec::chain_spec::ChainSpec"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
