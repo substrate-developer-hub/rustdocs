a059e7304aa587f4ab07e1d3f40202b2d2db74a4
devops-parity <devops-team@parity.io>
2019-10-01 09:42:56 +0000

    update rustdoc master

--- master/substrate_transaction_pool/sidebar-items.js
+++ master/substrate_transaction_pool/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["error","Transaction pool error."]],"struct":[["ChainApi","The transaction pool logic"]]});
+initSidebarItems({"mod":[["error","Transaction pool error."]],"struct":[["FullChainApi","The transaction pool logic"]]});
