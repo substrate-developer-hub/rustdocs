16329a6013e2c03a78e4fad6af300cc4444494cd
devops-parity <devops-team@parity.io>
2019-10-10 11:22:56 +0000

    update rustdoc master

---
+++ master/implementors/srml_support/traits/trait.Randomness.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["srml_randomness_collective_flip"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"srml_system/trait.Trait.html\" title=\"trait srml_system::Trait\">Trait</a>&gt; <a class=\"trait\" href=\"srml_support/traits/trait.Randomness.html\" title=\"trait srml_support::traits::Randomness\">Randomness</a>&lt;&lt;T as <a class=\"trait\" href=\"srml_system/trait.Trait.html\" title=\"trait srml_system::Trait\">Trait</a>&gt;::<a class=\"type\" href=\"srml_system/trait.Trait.html#associatedtype.Hash\" title=\"type srml_system::Trait::Hash\">Hash</a>&gt; for <a class=\"struct\" href=\"srml_randomness_collective_flip/struct.Module.html\" title=\"struct srml_randomness_collective_flip::Module\">Module</a>&lt;T&gt;",synthetic:false,types:["srml_randomness_collective_flip::Module"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
