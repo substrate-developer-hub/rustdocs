2178f17596181b16e90dbf71e704e221a9fcfb34
devops-parity <devops-team@parity.io>
2019-10-24 16:03:56 +0000

    update rustdoc master

--- master/srml_treasury/sidebar-items.js
+++ master/srml_treasury/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Call",""],["RawEvent","Events for this module."]],"struct":[["Module",""],["Proposal","A spending proposal."]],"trait":[["Trait",""]],"type":[["Event","[`RawEvent`] specialized for the configuration [`Trait`]"]]});
+initSidebarItems({"enum":[["Call",""],["RawEvent","Events for this module."]],"struct":[["GenesisConfig",""],["Module",""],["Proposal","A spending proposal."]],"trait":[["Trait",""]],"type":[["Event","[`RawEvent`] specialized for the configuration [`Trait`]"]]});
