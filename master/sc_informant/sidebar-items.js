2acba6739ca7d151d0bbd7d952b64de6ca8c7793
devops-parity <devops-team@parity.io>
2020-02-10 11:57:21 +0000

    update rustdoc master

---
+++ master/sc_informant/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["OutputFormat","The format to print telemetry output in."]],"fn":[["build","Creates an informant in the form of a `Future` that must be polled regularly."]]});
