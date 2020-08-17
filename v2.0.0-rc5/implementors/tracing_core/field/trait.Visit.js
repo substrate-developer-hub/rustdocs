7908f2af00626dd117b2f9ef76405d2ccc893f97
Dan Forbes <dan@danforbes.dev>
2020-08-17 16:16:59 -0700

    Add Google Analytics header and instructions. Update to v2.0.0-rc5+2 that was built with Google Analytics.

--- v2.0.0-rc5/implementors/tracing_core/field/trait.Visit.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {};
-implementors["sc_tracing"] = [{"text":"impl <a class=\"trait\" href=\"tracing_core/field/trait.Visit.html\" title=\"trait tracing_core::field::Visit\">Visit</a> for <a class=\"struct\" href=\"sc_tracing/struct.Values.html\" title=\"struct sc_tracing::Values\">Values</a>","synthetic":false,"types":["sc_tracing::Values"]}];
-implementors["tracing_core"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
